cdcbb3c49c540696e6dfd7f19527e2048ce02c0f
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-10-15 15:13:14 +0530

    removed editor api

--- src/Components/RTE.jsx
+++ src/Components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
 import {Controller} from 'react-hook-form'
+import config from '../config/config'
 
 export default function RTE({name, control, label, defaultValue= ""}) {
   return (
@@ -12,7 +13,7 @@ export default function RTE({name, control, label, defaultValue= ""}) {
     control={control} 
     render={({field: {onChange}})=>(
         <Editor
-        apiKey='xn7d6fc8t7vbqs4zueigcamnbleygt15fiejx9he7rnllzbu'
+        apiKey={config.tinyMceApi}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,8 @@ const config = {
     appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+    tinyMceApi: String(import.meta.env.VITE_EDITOR_API)
 }
 
 export default config;
