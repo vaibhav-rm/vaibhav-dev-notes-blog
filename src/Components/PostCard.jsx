20f773140a47a07d2f4fe39f45814e2d933c6105
Appwrite <team@appwrite.io>
2024-10-16 14:24:49 +0000

    Create 'getUsernameFromUserId' function

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -19,7 +19,7 @@ function PostCard({ $id, title, featuredImage, content, $createdAt, userId }) {
         setAuthor(user.name);
       }).catch((error) => {
         console.error("Error fetching author data:", error);
-        setAuthor("Unknown"); // Fallback in case of an error
+        setAuthor("Unknown Author");
       });
     }
   }, [content, userId]);
