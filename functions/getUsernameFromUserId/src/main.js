43d545061fe9d8dc357e3ac97aa77102522ce2dc
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-10-21 23:18:08 +0530

    function config ugs

--- functions/getUsernameFromUserId/src/main.js
+++ functions/getUsernameFromUserId/src/main.js
@@ -1,28 +1,36 @@
 import { Client, Users } from 'node-appwrite';
+import config from '../../../src/config/config'
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
+  // Initialize the Appwrite client
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setEndpoint(config.appwriteUrl)
+    .setProject(config.appwriteProjectId)
     .setKey(req.headers['x-appwrite-key'] ?? '');
   const users = new Users(client);
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+  // Extract userId from request parameters
+  const userId = req.body.userId ?? req.headers['x-user-id'] ?? '';
+
+  if (!userId) {
+    return res.json({ error: "UserId is required." });
   }
 
-  // The req object contains the request data
+  try {
+    // Fetch the user by userId
+    const user = await users.get(userId);
+    log(`User found: ${user.name}`);
+
+    // Return the user object
+    return res.json(user);
+  } catch (err) {
+    error("Could not retrieve user: " + err.message);
+    return res.json({ error: "User not found: " + err.message });
+  }
+
+  // Example response for /ping path
   if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
     return res.text("Pong");
   }
 
