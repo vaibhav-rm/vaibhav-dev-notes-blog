c2eb5b832b4557f8c423ef946465bd834d8f4fd8
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-10-22 22:40:24 +0530

    added logs

--- functions/getUsernameFromUserId/src/main.js
+++ functions/getUsernameFromUserId/src/main.js
@@ -19,6 +19,7 @@ export default async ({ req, res, log, error }) => {
 
     // Validate that userId is provided
     if (!userId) {
+      log(userId);
       error("userId is required");
       return res.json({ error: "userId is required" });
     }
